a493a967622899dfa055e0099b7eb4ae6a0e9808
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2022-12-19 12:51:36 +0900

    gameRuleの中のGameOfLifeクラスのcreateGliderメソッドをデフォルト引数を反映するように書き換えた

--- src/gameRule.py
+++ src/gameRule.py
@@ -4,6 +4,8 @@
 from copy import deepcopy
 
 # Game rules
+
+
 class GameOfLife:
     """
     このライフゲームの要となる法則を定義している。
@@ -12,7 +14,7 @@ class GameOfLife:
     def __init__(self, world_size: tuple):
         #世界の大きさdefoultで10
         self.world_size = world_size
-        self.allDeath() # ここでself.worldクラス変数が定義される
+        self.allDeath()  # ここでself.worldクラス変数が定義される
         self.tmp_world = deepcopy(self.world)
 
     def allDeath(self):
@@ -20,7 +22,6 @@ class GameOfLife:
         """
         self.world = [[False for _ in range(
             self.world_size[0])] for _ in range(self.world_size[1])]
-
 
     def update(self):
         """世代交代
@@ -30,19 +31,20 @@ class GameOfLife:
                 self.tmp_world[y][x] = self.judge(x, y)
         self.world = deepcopy(self.tmp_world)
 
-    def createGlider(self, x = 0,y=0):
+    def createGlider(self, x=0, y=0):
         """conway's game of life における グライダーを作成する
+        x,yはworldの中のy行x列の座標を表す
+        x,yはこのメソッドが作成するべきグライダーの左上の座標を示している
         """
-
-        self.world[1][1] = True
-        self.world[1][2] = False
-        self.world[1][3] = True
-        self.world[2][1] = False
-        self.world[2][2] = True
-        self.world[2][3] = True
-        self.world[3][1] = False
-        self.world[3][2] = True
-        self.world[3][3] = False
+        self.world[y+1][x+1] = True
+        self.world[y+1][x+2] = False
+        self.world[y+1][x+3] = True
+        self.world[y+2][x+1] = False
+        self.world[y+2][x+2] = True
+        self.world[y+2][x+3] = True
+        self.world[y+3][x+1] = False
+        self.world[y+3][x+2] = True
+        self.world[y+3][x+3] = False
 
     def judge(self, x, y):
         """次の時代lifeならTrueを返すdeathならFalseをかえす
@@ -58,15 +60,14 @@ class GameOfLife:
             if count == 3:
                 return True
             return False
-        
 
     def countCells(self, x, y):
         """周辺の状態をカウントする
         """
         count = 0
 
-        for yi in range(-1,2):
-            for xi in range(-1,2):
+        for yi in range(-1, 2):
+            for xi in range(-1, 2):
                 if 0 == xi and 0 == yi:
                     continue
                 if self.world[(y+yi) % self.world_size[1]][(x+xi) % self.world_size[0]] == True:
